e4a4ef6f870aad64439ebf73d21cb7c0bf99488c
dennomwas <githinji.mwangi@gmail.com>
2018-09-23 11:57:27 +0300

    [Ft] add get user profile

---
+++ profile.js
@@ -0,0 +1,43 @@
+// Problem: We need a simple way to look at a user's badge count and JavaScript points
+// Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
+const https = require('https');
+const http = require('http');
+
+// local imports
+const printMessage = require('./print.js');
+const PrintError = require('./printerrors.js');
+
+const get = username => {
+    try {
+        // connect to teamtreehouse api url(https://teamtreehouse.com/dennismwangi2.json)
+        const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+            if (response.statusCode === 200) {
+                let responseBody = "";
+
+                // read the data
+                response.on("data", data => {
+                    responseBody += data.toString();
+                });
+
+                // parse to json and print the data
+                response.on("end", () => {
+                    try {
+                        const profile = JSON.parse(responseBody);
+                        printMessage.print(username, profile.badges.length, profile.points.JavaScript);
+                    } catch (error) {
+                        PrintError.errorReport(error);
+                    }
+                });
+            } else {
+                const message = `There was an error fetching the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
+                const statusCodeErr = new Error(message)
+                PrintError.errorReport(statusCodeErr);
+            }
+        });
+        request.on("error", error => PrintError.errorReport(error));
+    } catch (error) {
+        PrintError.errorReport(error);
+    }
+} // end getProfile
+
+module.exports.get = get;
